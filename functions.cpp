7e942e4b67c4c8b4bfcfdb212e39acc73c9c1b99
henrikasz1 <58070388+henrikasz1@users.noreply.github.com>
2020-10-07 19:24:10 +0300

    Delete functions.cpp

--- functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "functions.h"
-
-void readFile (std::string fileName, std::string &data)
-{
-  std::ifstream in (fileName);
-  std::stringstream a;
-  a << in.rdbuf();
-  data = a.str();
-}
